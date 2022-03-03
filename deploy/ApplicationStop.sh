209882f301d38f3031cc28ead7703dd9756c9b40
Pedro Cruz <pcruzh@gmail.com>
2022-03-02 20:15:54 -0500

    agrego ApplicationStop

--- deploy/ApplicationStop.sh
+++ deploy/ApplicationStop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Delete service
 
-cd /home/app/serverExpress/
+cd /home/app/serverExpress
 # forever-service delete serverExpress
--- deploy/BeforeInstall.sh
+++ deploy/BeforeInstall.sh
@@ -2,7 +2,7 @@
 
 #update the system
 
-rm -rf /home/app/serverExpress
+sudo rm -rf /home/app/serverExpress
 
 sudo yum update
 sudo yum upgrade
